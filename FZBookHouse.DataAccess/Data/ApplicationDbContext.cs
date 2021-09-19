532900a91434fc349da7d2ce803b23741483113c
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2021-09-20 02:28:37 +0600

    Add dropdown & Category table into DB

--- FZBookHouse.DataAccess/Data/ApplicationDbContext.cs
+++ FZBookHouse.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FZBookHouse.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ namespace FZBookHouse.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+      
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
